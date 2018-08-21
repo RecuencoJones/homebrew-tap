f82d325331648a42071f97fd6185879ca9b50e0c
David Recuenco <david.recuenco@adidas-group.com>
2018-08-21 08:50:35 +0200

    ADD sha256 to npmrc

--- npmrc.rb
+++ npmrc.rb
@@ -3,6 +3,7 @@ class Npmrc < Formula
   desc "A CLI to manage multiple npmrc files"
   homepage "https://github.com/RecuencoJones/npmrc"
   url "https://github.com/RecuencoJones/npmrc/releases/download/#{version}/darwin_amd64.tar.gz"
+  sha256 "6c2ff515e1a38d5dc71af89da8471d7c532080dc3ad44dc7984b5286aca4d00c"
 
   def install
       system "mv darwin_amd64/npmrc npmrc"
