f5d66d0b4951169815e8607cd596bdba5fe089be
David Recuenco <david.recuenco@adidas-group.com>
2018-08-21 13:14:29 +0200

    UPDATE npmrc formula

--- npmrc.rb
+++ npmrc.rb
@@ -1,8 +1,8 @@
 class Npmrc < Formula
-  version = "0.1.3"
+  version "0.1.3"
   desc "A CLI to manage multiple npmrc files"
   homepage "https://github.com/RecuencoJones/npmrc"
-  url "https://github.com/RecuencoJones/npmrc/releases/download/#{version}/darwin_amd64.tar.gz"
+  url "https://github.com/RecuencoJones/npmrc/releases/download/#{self.version}/darwin_amd64.tar.gz"
   sha256 "719aa14d3d4db588eaaa72d18d3491c32f85ea204a429836194c6371f0f5ba59"
 
   def install
@@ -11,7 +11,7 @@ class Npmrc < Formula
   end
 
   test do
-    system "#{bin}/npmrc", "help"
+    assert_match shell_output("#{bin}/npmrc version").strip, version
   end
 end
 
